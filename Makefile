944e2992b3f4334d7b71240cafae66a2139ffea0
Barry <rountree4@llnl.gov>
2023-03-23 08:52:46 -0700

    Moving over to tioga.

--- Makefile
+++ Makefile
@@ -1,12 +1,15 @@
 all: parse_elf 0
 
 parse_elf: parse_elf.c Makefile
-	clang-13 -std=c2x -O3 -Wall -Wextra -Werror -o parse_elf parse_elf.c
+	clang -std=c2x -O3 -Wall -Wextra -Werror -o parse_elf parse_elf.c
 
 0: 0.c Makefile
-	clang-13 -S 0.c
-	clang-13 -g -s -static -nostartfiles -nodefaultlibs -Os -o 0 0.s
+	clang -S 0.c
+	clang -g -s -static -nostartfiles -nodefaultlibs -nostdlib -Os -o 0 0.s
 	# If using gcc ok to add -nostdlib.  Not supported with clang-13.
+
+run:
+	./parse_elf ./0
 
 clean:
 	rm -f parse_elf 0
--- parse_elf.c
+++ parse_elf.c
@@ -393,15 +393,15 @@ parse_program_headers(){
             e->e_phoff, e->e_phnum, e->e_phentsize);
 
     // Program header index
-    printf("%6s %19s %5s %15s %15s %15s %15s %15s %15s\n",
-            "index","type","perms","offset", "vaddr", "paddr", "filesz", "memsz", "align");
-    printf("%6s %19s %5s %15s %15s %15s %15s %15s %15s\n",
-        "======","==================","=====","===============","===============","===============","===============","===============","===============");
+    printf("%6s %6s %19s %5s %15s %15s %15s %15s %15s %15s\n",
+            "offset", "index","type","perms","offset", "vaddr", "paddr", "filesz", "memsz", "align");
+    printf("%6s %6s %19s %5s %15s %15s %15s %15s %15s %15s\n",
+        "======","======","==================","=====","===============","===============","===============","===============","===============","===============");
     for(uint16_t i=0; i<e->e_phnum; i++, ph++){
-
-        //     index        type perms       offset       vaddr        paddr        filesz       memsz        align
+        //     offset   index        type perms       offset       vaddr        paddr        filesz       memsz        align
     	snprintf(err_buf, ERR_BUF_SZ, "Invalid:(%#"PRIx32")\n", ph->p_type);
-        printf("%#6"PRIx16" %19s %3c%1c%1c %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64"\n",
+        printf("%#06lx %#6"PRIx16" %19s %3c%1c%1c %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64" %#15"PRIx64"\n",
+                e->e_phoff + ( i * e->e_phentsize ),
                 i,                                                      // index
                 ph->p_type == PT_NULL ? "NULL" :                        // type
                 ph->p_type == PT_LOAD ? "LOAD" :
@@ -427,6 +427,18 @@ parse_program_headers(){
                 ph->p_align                                             // align
                 );
     }
+    printf("\n\n");
+}
+
+void
+parse_section_headers(){
+    Elf64_Ehdr *e = (Elf64_Ehdr *)map_addr;
+//    Elf64_Phdr *sh = (Elf64_Phdr*)(map_addr+(e->e_shoff));
+
+    printf("Section headers\n");
+    printf("\tStart = %#"PRIx64", Count = %#"PRIx16", Size (each)=%#"PRIx16"\n\n",
+            e->e_shoff, e->e_shnum, e->e_shentsize);
+
 }
 
 int
@@ -435,6 +447,7 @@ main( [[maybe_unused]] int argc, [[maybe_unused]] char **argv ){
     map_file();
     parse_elf_header();
     parse_program_headers();
+    parse_section_headers();
     cleanup();
     return 0;
 }
